bc050a85961fc867f724584cf93495bbcae94bd2
rsliapukhinjs <rsliapukhinjs@gmail.com>
2024-03-06 19:19:14 +0300

    finish exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
+
+const morgan = require("morgan");
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
